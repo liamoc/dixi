5d5b6f9478c7da0e359c6e6671a070e7f9d1a5ab
Liam O'Connor <liamoc@cse.unsw.edu.au>
2015-10-30 17:51:44 +1100

    Add typeable to dixiError for 7.8

--- Dixi/Common.hs
+++ Dixi/Common.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Dixi.Common where
 
 import Data.Text
+import Data.Typeable
 
 type Key = Text
 type Version = Int
 
 data DixiError = VersionNotFound Key Version
                | PatchNotApplicable Key
+               deriving (Show, Typeable)
